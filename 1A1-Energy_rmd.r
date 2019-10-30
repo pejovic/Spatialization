68d3d703459509b6338841a2897cf392b57dc320
Milutin Pejovic <milutinpejovic@gmail.com>
2019-10-30 22:58:43 +0100

    Calculation for 1A1 done

--- 1A1-Energy_rmd.r
+++ 1A1-Energy_rmd.r
@@ -63,25 +63,30 @@ mycolors=c("#f32440","#2185ef","#d421ef")
 #+ include = FALSE, echo = FALSE
 # source.list = source.1A1a
 corsum2sf <- function(source.list){
+  source.list$sources$points[, vars] <- source.list$sources$points[, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  source.list[[2]][[2]][, vars] <- source.list[[2]][[2]][, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  
   points.sum <- source.list$sources$points %>% 
     dplyr::select(., vars) %>% 
-    dplyr::select_if(., is.numeric) %>% 
     apply(., 2, sum) %>% 
     t(.) %>% 
     as.data.frame() %>%
     dplyr::mutate_if(is.numeric, round, 2)
   
-  points.total <- source.list[[2]][[2]][, names(points.sum)] %>% 
-    mutate_all(~replace(., is.na(.), 0)) %>%
+  points.total <- source.list[[2]][[2]][, vars] %>% 
     dplyr::mutate_if(is.numeric, round, 2) %>%
     as.data.frame()
   
   if(!identical(points.sum, points.total)){
-    source.diff.vars <- names(points.sum)[!(points.total == points.sum)]
     d <- (points.total - points.sum)[1, ]
-    w <- apply(source.1A1c$sources$points[,source.diff.vars], 2, function(x) x/sum(x))
-    cor.data <- w %*% diag(d) + source.list$sources$points[, source.diff.vars]
-    source.list$sources$points[, source.diff.vars] <- cor.data
+    zero.ind <- source.list$sources$points[, vars] == 0
+    w <- replace(source.list$sources$points[, vars], zero.ind, NA) %>% 
+      apply(., 2, function(x) x/sum(x)) %>%
+      as.data.frame() %>%
+      dplyr::mutate_all(~replace(., is.na(.), 0))
+      
+    cor.data <- as.matrix(w) %*% diag(d) + source.list$sources$points[, vars]
+    source.list$sources$points[, vars] <- cor.data
     source.sf <- st_as_sf(x = source.list$sources$points, coords = c("Longitude", "Latitude"),  crs = "+proj=longlat +datum=WGS84")
   }else{
     source.sf <- st_as_sf(x = source.list$sources$points, coords = c("Longitude", "Latitude"),  crs = "+proj=longlat +datum=WGS84")
@@ -116,7 +121,6 @@ sf.1A1a <- corsum2sf(source.1A1a)
 #+ echo = FALSE, result = TRUE, eval = TRUE
 sf.1A1a %>% 
   st_drop_geometry() %>% 
-  #dplyr::select_if(., is.numeric) %>%
   dplyr::mutate_if(is.numeric, round, 2) %>%
   datatable(., caption = 'Table 1: sf.1A1a',
             rownames = FALSE, escape = FALSE, selection = "single",
@@ -136,17 +140,16 @@ sf.1A1a %>%
 sum.1A1a <- sf.1A1a %>% 
   st_drop_geometry() %>%
   dplyr::select(., vars) %>% 
-  dplyr::select_if(., is.numeric) %>% 
   apply(., 2, sum) %>% 
   t(.) %>% 
   as.data.frame() %>%
   dplyr::mutate_if(is.numeric, round, 2)
-total.1A1a <- source.1A1a[[2]][[2]][, names(points.sum)] %>% 
+total.1A1a <- source.1A1a[[2]][[2]][, vars] %>% 
   mutate_all(~replace(., is.na(.), 0)) %>%
   dplyr::mutate_if(is.numeric, round, 2) %>%
   as.data.frame()
 
-data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1A1a, total.1A1a, data.frame(sum.1A1a[, names(total.1A1b)] == total.1A1a)-1)) %>%
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1A1a, total.1A1a, data.frame(sum.1A1a == total.1A1a)-1)) %>%
   datatable(., caption = 'Table 2: Summary differences',
             options = list(pageLength = 5)
   )
@@ -169,7 +172,6 @@ sf.1A1b <- corsum2sf(source.1A1b)
 #+ echo = FALSE, result = TRUE, eval = TRUE
 sf.1A1b %>% 
   st_drop_geometry() %>% 
-  #dplyr::select_if(., is.numeric) %>%
   dplyr::mutate_if(is.numeric, round, 2) %>%
   datatable(., caption = 'Table 3: sf.1A1b',
             rownames = FALSE, escape = FALSE, selection = "single",
@@ -189,21 +191,231 @@ sf.1A1b %>%
 sum.1A1b <- sf.1A1b %>% 
   st_drop_geometry() %>%
   dplyr::select(., vars) %>% 
-  dplyr::select_if(., is.numeric) %>% 
   apply(., 2, sum) %>% 
   t(.) %>% 
   as.data.frame() %>%
   dplyr::mutate_if(is.numeric, round, 2)
-total.1A1b <- source.1A1b[[2]][[2]][, names(points.sum)] %>% 
+total.1A1b <- source.1A1b[[2]][[2]][, vars] %>% 
   mutate_all(~replace(., is.na(.), 0)) %>%
   dplyr::mutate_if(is.numeric, round, 2) %>%
   as.data.frame()
 
-data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1A1b[, names(total.1A1b)], total.1A1b, data.frame(sum.1A1b[, names(total.1A1b)] == total.1A1b)-1)) %>%
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1A1b, total.1A1b, data.frame(sum.1A1b == total.1A1b)-1)) %>%
   datatable(., caption = 'Table 4: Summary differences',
             options = list(pageLength = 5)
   )
 
+#'
+#'
+#' ## 1B2aiv - Fugitive emissions from liquid fuels: Refining, storage
+#'
+#+ include = FALSE
+
+source.1B2aiv <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1B2aiv$sources$points <- readxl::read_xlsx(path = source.file, range = "D53:S56", sheet = source.sheet, col_names = header)
+source.1B2aiv$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D57:I57", sheet = source.sheet, col_names = vars)
+source.1B2aiv$total$inventory <- readxl::read_xlsx(path = source.file, range = "D58:I58", sheet = source.sheet, col_names = vars)
+
+sf.1B2aiv <- corsum2sf(source.1B2aiv)
+
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sf.1B2aiv %>% 
+  st_drop_geometry() %>% 
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  datatable(., caption = 'Table 5: sf.1A2aiv',
+            rownames = FALSE, escape = FALSE, selection = "single",
+            extensions = c('Buttons'),
+            class = 'white-space: nowrap',
+            options = list(
+              pageLength = 5,
+              dom = 'Bfrtip',
+              buttons = list('pageLength'),
+              searchHighlight = TRUE,
+              scrollX = TRUE,
+              scrollY = TRUE
+            ))
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sum.1B2aiv <- sf.1B2aiv %>% 
+  st_drop_geometry() %>%
+  dplyr::select(., vars) %>% 
+  apply(., 2, sum) %>% 
+  t(.) %>% 
+  as.data.frame() %>%
+  dplyr::mutate_if(is.numeric, round, 2)
+total.1B2aiv <- source.1B2aiv[[2]][[2]][, vars] %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  as.data.frame()
+
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1B2aiv, total.1B2aiv, data.frame(sum.1B2aiv == total.1B2aiv)-1)) %>%
+  datatable(., caption = 'Table 6: Summary differences',
+            options = list(pageLength = 5)
+  )
+#'
+#'
+#'
+#'
+#'
+#'
+#' ## 1B2c - Fugitive emissions: Venting and flaring
+#' 
+#+ include = FALSE
+source.1B2c <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1B2c$sources$points <- readxl::read_xlsx(path = source.file, range = "D59:S62", sheet = source.sheet, col_names = header)
+source.1B2c$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D63:I63", sheet = source.sheet, col_names = vars)
+source.1B2c$total$inventory <- readxl::read_xlsx(path = source.file, range = "D64:I64", sheet = source.sheet, col_names = vars)
+
+sf.1B2c <- corsum2sf(source.1B2c)
+#'
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sf.1B2c %>% 
+  st_drop_geometry() %>% 
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  datatable(., caption = 'Table 5: sf.1B2c',
+            rownames = FALSE, escape = FALSE, selection = "single",
+            extensions = c('Buttons'),
+            class = 'white-space: nowrap',
+            options = list(
+              pageLength = 5,
+              dom = 'Bfrtip',
+              buttons = list('pageLength'),
+              searchHighlight = TRUE,
+              scrollX = TRUE,
+              scrollY = TRUE
+            ))
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sum.1B2c <- sf.1B2c %>% 
+  st_drop_geometry() %>%
+  dplyr::select(., vars) %>% 
+  apply(., 2, sum) %>% 
+  t(.) %>% 
+  as.data.frame() %>%
+  dplyr::mutate_if(is.numeric, round, 2)
+total.1B2c <- source.1B2c[[2]][[2]][, vars] %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  as.data.frame()
+
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1B2c, total.1B2c, data.frame(sum.1B2c == total.1B2c)-1)) %>%
+  datatable(., caption = 'Table 6: Summary differences',
+            options = list(pageLength = 5)
+  )
+#'
+#'
+#'
+#' ## 1A1c - Manufacturing of solid fuels
+#'
+#+ include = FALSE
+source.1A1c <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1A1c$sources$points <- readxl::read_xlsx(path = source.file, range = "D65:S73", sheet = source.sheet, col_names = header)
+source.1A1c$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D81:I81", sheet = source.sheet, col_names = vars)
+source.1A1c$total$inventory <- readxl::read_xlsx(path = source.file, range = "D82:I82", sheet = source.sheet, col_names = vars)
+
+sf.1A1c <- corsum2sf(source.1A1c)
+
+#'
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sf.1A1c %>% 
+  st_drop_geometry() %>% 
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  datatable(., caption = 'Table 7: sf.1A1c',
+            rownames = FALSE, escape = FALSE, selection = "single",
+            extensions = c('Buttons'),
+            class = 'white-space: nowrap',
+            options = list(
+              pageLength = 5,
+              dom = 'Bfrtip',
+              buttons = list('pageLength'),
+              searchHighlight = TRUE,
+              scrollX = TRUE,
+              scrollY = TRUE
+            ))
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sum.1A1c <- sf.1A1c %>% 
+  st_drop_geometry() %>%
+  dplyr::select(., vars) %>% 
+  apply(., 2, sum) %>% 
+  t(.) %>% 
+  as.data.frame() %>%
+  dplyr::mutate_if(is.numeric, round, 2)
+total.1A1c <- source.1A1c[[2]][[2]][, vars] %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  as.data.frame()
+
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1A1c, total.1A1c, data.frame(sum.1A1c == total.1A1c)-1)) %>%
+  datatable(., caption = 'Table 8: Summary differences',
+            options = list(pageLength = 5)
+  )
+#'
+#'
+#'
+#'
+#'
+#'
+#' ## 1B1b - Fugitive emissions from solid fuels: Solid fuel transformation
+#'
+#+ include = FALSE
+source.1B1b <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1B1b$sources$points <- readxl::read_xlsx(path = source.file, range = "D83:S83", sheet = source.sheet, col_names = header)
+source.1B1b$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D91:I91", sheet = source.sheet, col_names = vars)
+source.1B1b$total$inventory <- readxl::read_xlsx(path = source.file, range = "D92:I92", sheet = source.sheet, col_names = vars)
+
+sf.1B1b <- corsum2sf(source.1B1b)
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sf.1B1b %>% 
+  st_drop_geometry() %>% 
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  datatable(., caption = 'Table 9: sf.1B1b',
+            rownames = FALSE, escape = FALSE, selection = "single",
+            extensions = c('Buttons'),
+            class = 'white-space: nowrap',
+            options = list(
+              pageLength = 5,
+              dom = 'Bfrtip',
+              buttons = list('pageLength'),
+              searchHighlight = TRUE,
+              scrollX = TRUE,
+              scrollY = TRUE
+            ))
+#'
+#'
+#+ echo = FALSE, result = TRUE, eval = TRUE
+sum.1B1b <- sf.1B1b %>% 
+  st_drop_geometry() %>%
+  dplyr::select(., vars) %>% 
+  apply(., 2, sum) %>% 
+  t(.) %>% 
+  as.data.frame() %>%
+  dplyr::mutate_if(is.numeric, round, 2)
+total.1B1b <- source.1B1b[[2]][[2]][, vars] %>% 
+  mutate_all(~replace(., is.na(.), 0)) %>%
+  dplyr::mutate_if(is.numeric, round, 2) %>%
+  as.data.frame()
+
+data.frame(sum = c("spatialize", "total", "diff"), rbind(sum.1B1b, total.1B1b, data.frame(sum.1B1b == total.1B1b)-1)) %>%
+  datatable(., caption = 'Table 10: Summary differences',
+            options = list(pageLength = 5)
+  )
+#'
+#'
+#'
 #'
 #'
 #'
