5dfd1fc4159ae23ad9b47b130da33160668920b7
BursacPetar <bursac.petar3@gmail.com>
2019-11-28 16:21:31 +0100

    ...

--- 1A4-Residential-Tertiary.r
+++ 1A4-Residential-Tertiary.r
@@ -1,6 +1,8 @@
 #' ---
 #' title: "Pollutant inventory spatialization"
-#' author: GILAB team
+#' author:
+#'    - "Milan Kilibarda"
+#'    - "Dragutin Protic"
 #' date: "`r format(Sys.time(), '%d %B %Y')`"
 #' output:   
 #'    html_document:
@@ -13,13 +15,14 @@
 #'      fig_caption: yes
 #' ---
 #' 
+#+ include = FALSE 
+# <img src="Logo-Trans.png" align="center" alt="logo" width="2000" height = "3000" style = "border: none; float: right;">
 #' 
-#' # 1A3 - Transport
+#' # 1A4-Residential-Tertiary
 #' This document provides the methodlogy and the main results regarding the spatialization of pollutation inventory.
 #' 
 #' 
 #+ include = TRUE, echo = FALSE, results = 'hide', warning = FALSE, message = FALSE
-library(XLConnect)
 library(tidyverse)
 library(sf)
 library(readxl)
@@ -29,48 +32,220 @@ library(here)
 library(knitr)
 library(kableExtra)
 library(DT)
+library(mapview)
+library(rgdal)
+library(SerbianCyrLat)
 #' 
 #' 
-#' ## Methodology: 
-#' 
-#' ### Auxiliary Data to be used:
-#' - The road network, including 1A, 2A, 1B and 2B road category [^1], 
-#' - Data of the Mean Daily Trafic for one Year (MDTY) obtained from ~400 automatic Vehicle Counting Devices (VCD), spreaded over the whole territory of Serbia, on three different road category (1A, 2A and 1B) [^1],
-#' - Number of vehicles registered in each municipality of the Serbia for the year 2015 [^2].
-#' - The map of Serbia with municipalities [^3]
-#' - Road network within the urban areas [^4]
-#' - Corine Land Cover for the territory of Serbia [^5]
-#' 
-#' [^1]: Source: Public Enterprise “Roads of Serbia”
-#' [^2]: Source: Statistical Office of the Republic of Serbia.
-#' [^3]: Source: GADM, the Database of Global Administrative Areas.
-#' [^4]: Source: Open Street Maps
-#' [^5]: Source: Corine Land Cover.
-#' 
-#' ### Methodology for road transport (1A3b)[^6] includes the following steps:
-#' 
-#' [^6]: Road transport that includes the urban/rural/highway inventory separately
-#' 
-#' 
-#' #### Rural inventory
-#' 1. Spatial classification of the whole Territory of Serbia into urban and rural areas based on Corine Land Cover data.
-#' 2. Spatial interpolation of the MDTY values (for each vehicle category) from VCDs at the mid-points of the roads where VCD not exist. This will be done for each category of road, separately, by using the knn method (k-nearest neighbors). In doing so, the distances to the whole road section where AVC exist will be taken into account. Model will be trained based through the cross-validation procedure, by using the RMSE (Root Mean Squared Error) as a measure of performace. In this way, the vehicle activity for each road will be estimated.
-#' 3. Based on these estimates, total rural inventory for each vehicle category will be spatially dissagregated into the 5x5km cells propotionally, by taking the estimated MDTY and the lengths of corresponding road sections that lie within the cell into account.
-#' 
+#+ include = FALSE
+# Plot settings
+my_theme <- function(base_size = 10, base_family = "sans"){
+  theme_minimal(base_size = base_size, base_family = base_family) +
+    theme(
+      axis.text = element_text(size = 10),
+      axis.text.x = element_text(angle = 0, vjust = 0.5, hjust = 0.5),
+      axis.title = element_text(size = 12),
+      panel.grid.major = element_line(color = "grey"),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "#fffcfc"),
+      strip.background = element_rect(fill = "#820000", color = "#820000", size =0.5),
+      strip.text = element_text(face = "bold", size = 10, color = "white"),
+      legend.position = "bottom",
+      legend.justification = "center",
+      legend.background = element_blank(),
+      panel.border = element_rect(color = "grey30", fill = NA, size = 0.5)
+    )
+}
+
+theme_set(my_theme())
+mycolors=c("#f32440","#2185ef","#d421ef")
+
+#'  
+#'    
+#+ include = FALSE, echo = FALSE
+# Parameters:
+#  - distribute - TRUE or FALSE, depend on that if someone want to distribute total inventory emissions to sources, when difference exists
+corsum2sf <- function(source.list, distribute = FALSE){
+  source.list$sources$points[, vars] <- source.list$sources$points[, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  source.list[[2]][[2]][, vars] <- source.list[[2]][[2]][, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  
+  points.sum <- source.list$sources$points %>% 
+    dplyr::select(., vars) %>% 
+    apply(., 2, sum) %>% 
+    t(.) %>% 
+    as.data.frame() %>%
+    dplyr::mutate_if(is.numeric, round, 2)
+  
+  points.total <- source.list[[2]][[2]][, vars] %>% 
+    dplyr::mutate_if(is.numeric, round, 2) %>%
+    as.data.frame()
+  
+  if(!identical(points.sum, points.total) & distribute == TRUE){
+    d <- (points.total - points.sum)[1, ]
+    zero.ind <- source.list$sources$points[, vars] == 0
+    w <- replace(source.list$sources$points[, vars], zero.ind, 1) %>% 
+      apply(., 2, function(x) x/sum(x)) %>%
+      as.data.frame() %>%
+      dplyr::mutate_all(~replace(., is.na(.), 0))
+    
+    cor.data <- as.matrix(w) %*% diag(d) + source.list$sources$points[, vars]
+    source.list$sources$points[, vars] <- cor.data
+    source.sf <- st_as_sf(x = source.list$sources$points, coords = c("Longitude", "Latitude"),  crs = "+proj=longlat +datum=WGS84")
+  }else{
+    source.sf <- st_as_sf(x = source.list$sources$points, coords = c("Longitude", "Latitude"),  crs = "+proj=longlat +datum=WGS84")
+  }
+  return(source.sf)
+}
+#'    
+corsum2sf_polygon <- function(source.list, distribute = FALSE){
+  source.list$sources$polygon[, vars] <- source.list$sources$polygon[, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0)) %>% st_drop_geometry()
+  source.list[[2]][[2]][, vars] <- source.list[[2]][[2]][, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  
+  polygon.sum <- source.list$sources$polygon %>% 
+    st_drop_geometry() %>%
+    dplyr::select(., vars) %>% 
+    apply(., 2, sum) %>% 
+    t(.) %>% 
+    as.data.frame() %>%
+    dplyr::mutate_if(is.numeric, round, 2)
+  
+  polygon.total <- source.list[[2]][[2]][, vars] %>% 
+    dplyr::mutate_if(is.numeric, round, 2) %>%
+    as.data.frame()
+  
+  if(!identical(polygon.sum, polygon.total) & distribute == TRUE){
+    d <- (polygon.total - polygon.sum)[1, ]
+    zero.ind <- source.list$sources$polygon[, vars] %>% st_drop_geometry() == 0
+    w <- replace(source.list$sources$polygon[, vars] %>% st_drop_geometry(), zero.ind, 1) %>% 
+      apply(., 2, function(x) x/sum(x)) %>%
+      as.data.frame() %>%
+      dplyr::mutate_all(~replace(., is.na(.), 0))
+    
+    cor.data <- as.matrix(w) %*% diag(d) + source.list$sources$polygon[, vars]%>% st_drop_geometry()
+    source.list$sources$polygon[, vars] <- cor.data
+    source.sf <- source.list$sources$polygon
+  }else{
+    source.sf <- source.list$sources$polygon
+  }
+  return(source.sf)
+}
+
+corsum2sf_lines <- function(source.list, distribute = FALSE){
+  source.list$sources$lines[, vars] <- source.list$sources$lines[, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0)) %>% st_drop_geometry()
+  source.list[[2]][[2]][, vars] <- source.list[[2]][[2]][, vars] %>% dplyr::mutate_all(~replace(., is.na(.), 0))
+  
+  lines.sum <- source.list$sources$lines %>% 
+    st_drop_geometry() %>%
+    dplyr::select(., vars) %>% 
+    apply(., 2, sum) %>% 
+    t(.) %>% 
+    as.data.frame() %>%
+    dplyr::mutate_if(is.numeric, round, 2)
+  
+  lines.total <- source.list[[2]][[2]][, vars] %>% 
+    dplyr::mutate_if(is.numeric, round, 2) %>%
+    as.data.frame()
+  
+  if(!identical(lines.sum, lines.total) & distribute == TRUE){
+    d <- (lines.total - lines.sum)[1, ]
+    zero.ind <- source.list$sources$lines[, vars] %>% st_drop_geometry() == 0
+    w <- replace(source.list$sources$lines[, vars] %>% st_drop_geometry(), zero.ind, 1) %>% 
+      apply(., 2, function(x) x/sum(x)) %>%
+      as.data.frame() %>%
+      dplyr::mutate_all(~replace(., is.na(.), 0))
+    
+    cor.data <- as.matrix(w) %*% diag(d) + source.list$sources$lines[, vars]%>% st_drop_geometry()
+    source.list$sources$lines[, vars] <- cor.data
+    source.sf <- source.list$sources$lines
+  }else{
+    source.sf <- source.list$sources$lines
+  }
+  return(source.sf)
+}
+
+
+# Function for spatial data visualisation at web maps
+# Parameters:
+#    1. sf.sources -  sf object with sources to be spatialised
+#    2. sf.spatialised - sf object with polygons after spatialisation
+spatialised.mapview <- function(sf.sources, layer.name.1 = "", sf.spatialised, layer.name.2 = "", vars = vars, source.lines = FALSE){
+  sf.spatialised$Spatialised <- NA
+  sf.spatialised[ ,vars] %<>% st_drop_geometry() %>% dplyr::mutate_all(.,as.double)
+  sf.spatialised$Spatialised[sf.spatialised$NOx == 0 & sf.spatialised$SO2 == 0 & sf.spatialised$PM10 == 0 & sf.spatialised$PM2.5 == 0 & sf.spatialised$NMVOC == 0 & sf.spatialised$NH3 == 0] <- 0
+  sf.spatialised$Spatialised[sf.spatialised$NOx !=0 | sf.spatialised$SO2 !=0 | sf.spatialised$PM10 !=0 | sf.spatialised$PM2.5 !=0 | sf.spatialised$NMVOC !=0 | sf.spatialised$NH3 != 0] <- 1
+  
+  if(source.lines == FALSE){
+    web_map <- mapview(sf.spatialised, layer.name = layer.name.2, zcol = "Spatialised") + mapview(sf.sources, layer.name = layer.name.1, col.regions = "red")
+  }else {
+    web_map <- mapview(sf.sources, layer.name = layer.name.1, color = "red") +  mapview(sf.spatialised, layer.name = layer.name.2, zcol = "Spatialised")
+  }
+  return(web_map)
+}
+
 #'
-#' #### Urban inventory 
-#' 1. First, total inventory pollution will be spatially dissagregated based on the number of the vehicles registered in each municipality.
-#' 2. Next, estimated urban emissions in each municipality will be further spatialized into 5x5km cells based on the the lenght of the roads that lie in each cell of urban areas.
-#' 
-#' #### Highway inventory  
-#' 
-#' - Total inventory for highway transport will be spatially dissagregated based on the MDTY values and the lenght of the corresponding highway section that lie in each cell. 
-#'    
-#' #### Total inventory
-#' 
-#' - Total air pollution from the road transport in each cell will be estimated as the sum of rural, urban and highway inventory for each cell proportionally.     
-#'    
-#'    
-#+ include = FALSE 
-# rmarkdown::render(here::here("/1A3-Transport.r"), output_dir = here::here("/Reports"))
- 
+#'
+#+ include = FALSE
+source.file = "Pollutant inventory spatialized-d30102019.xlsx"
+source.sheet =  "1A4-Residential-Tertiary"
+header <- readxl::read_xlsx(path = source.file, range = "D8:S8", sheet = source.sheet) %>% names()
+vars <- header[1:6]
+grid.5km <- readOGR("Grid/Polygons_5km_UTM_34N.shp")
+sf.grid.5km <- st_as_sf(grid.5km) 
+
+#'
+#'
+#' ## 1A4ai - Commercial/Institutional: Stationary Combustion
+#'
+#'
+#'
+#+ include = FALSE
+source.1A4ai <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1A4ai$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D18:I18", sheet = source.sheet, col_names = vars)
+source.1A4ai$total$inventory <- readxl::read_xlsx(path = source.file, range = "D22:I22", sheet = source.sheet, col_names = vars)
+
+
+
+#'
+#'
+#' ## 1A4bi - Residential: Stationary combustion
+#'
+#'
+#'
+#+ include = FALSE
+source.1A4bi <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1A4bi$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D32:I32", sheet = source.sheet, col_names = vars)
+source.1A4bi$total$inventory <- readxl::read_xlsx(path = source.file, range = "D37:I37", sheet = source.sheet, col_names = vars)
+
+
+
+
+#'
+#'
+#' ## 1A4ci - Agriculture/Forestry/Fishing: Stationary combustion
+#'
+#'
+#'
+#+ include = FALSE
+source.1A4ci <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1A4ci$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D47:I47", sheet = source.sheet, col_names = vars)
+source.1A4ci$total$inventory <- readxl::read_xlsx(path = source.file, range = "D51:I51", sheet = source.sheet, col_names = vars)
+
+
+
+#'
+#'
+#' ## 1A4cii - Agriculture/Forestry/Fishing: Off-road vehicles and other machinery
+#'
+#'
+#'
+#+ include = FALSE
+source.1A4cii <- list(sources = list(points = NA, lines = NA, polygon = NA), total = list(spatialize = NA, inventory = NA))
+
+source.1A4cii$total$spatialize <- readxl::read_xlsx(path = source.file, range = "D61:I61", sheet = source.sheet, col_names = vars)
+source.1A4cii$total$inventory <- readxl::read_xlsx(path = source.file, range = "D64:I64", sheet = source.sheet, col_names = vars)
+
+
+
