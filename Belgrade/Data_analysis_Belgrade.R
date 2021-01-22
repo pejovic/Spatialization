39bfb7426a77e8db5d39f52b0efa43f3e6831205
BursacPetar <bursac.petar3@gmail.com>
2021-01-22 15:30:00 +0100

    ...

--- Belgrade/Data_analysis_Belgrade.R
+++ Belgrade/Data_analysis_Belgrade.R
@@ -51,7 +51,24 @@ sf_SO2 <- st_as_sf(edg_SO2, coords = c("lon", "lat"), crs = 4326)
 
 sf_NH3_bg <- sf_NH3[sf_opstine_bg, ]
 
+emep_grid <- st_read(dsn = "D:/Projekti/Spatialisation/OutputExamples-20191028T160417Z-001/OutputExamples/Sum-up and examples/1. Prior information/EMEP grid for Serbia/EMEP_GRID_01x01DEG_RS.shp")
+
 mapview(sf_NH3_bg, zcol = "emission 2015 (tons)") + mapview(sf_data_beograd) + mapview(emep_grid)
 
-emep_grid <- st_read(dsn = "D:/Projekti/Spatialisation/OutputExamples-20191028T160417Z-001/OutputExamples/Sum-up and examples/1. Prior information/EMEP grid for Serbia/EMEP_GRID_01x01DEG_RS.shp")
+
+
+edg_NH3_mov <- edg_NH3 %>% dplyr::mutate(lat = (lat - 0.05), 
+                                         lon = (lon + 0.05))
+
+
+sf_edg_NH3_mov <- st_as_sf(edg_NH3_mov, 
+                           coords = c("lon", "lat"), 
+                           crs = 4326)
+
+
+sf_edg_NH3_mov_bg <- sf_edg_NH3_mov[sf_opstine_bg, ]
+
+mapview(sf_edg_NH3_mov_bg, zcol = "emission 2015 (tons)") + mapview(sf_data_beograd) + mapview(emep_grid)
+
+
 
